fa598522955921ba9a7e6b5a0f1ff2dee0192906
imkashif <mohmdkashif@gmail.com>
2024-03-18 13:35:03 +0530

    altered app/schemas/predict.py

--- app/schemas/predict.py
+++ app/schemas/predict.py
@@ -22,20 +22,21 @@ class MultipleDataInputs(BaseModel):
             "example": {
                 "inputs": [
                     {
-                "PassengerId": [80], # datetime.datetime.strptime("2012-11-05", "%Y-%m-%d"),  
-                "Pclass": [2], 
-                "Name": ["Caldwell, Master. Alden Gates"],
-                "Sex": ['male'], 
-                "Age": [83],
-                "SibSp": [0],
-                "Parch": [2],
-                "Ticket": ['248738'],
-                "Cabin": [np.nan],
-                "Embarked": ['S'],	
-                "Fare": [30],
+                "PassengerId": 80, # datetime.datetime.strptime("2012-11-05", "%Y-%m-%d"),  
+                "Pclass": 2, 
+                "Name": "Caldwell, Master. Alden Gates",
+                "Sex": "male", 
+                "Age": 50,
+                "SibSp": 0,
+                "Parch": 2,
+                "Ticket": "248738",
+                "Cabin": "C123",
+                "Embarked": 'S',	
+                "Fare": 30,
                     }
                 ]
             }
         }
 #data_in={'PassengerId':[80],'Pclass':[2],'Name':["Caldwell, Master. Alden Gates"],'Sex':['male'],'Age':[0.83],
 #                'SibSp':[0],'Parch':[2],'Ticket':['248738'],'Cabin':[np.nan,],'Embarked':['S'],'Fare':[29]}
+        
